6da3657b63561e7403f8fd8fa3e49961b7b693b0
angel102004 <palaosorioangel2@gmail.com>
2025-08-20 22:00:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Ejemplo de mensaje de confirmación al enviar el formulario
+document.getElementById('reserve-form').addEventListener('submit', function(event) {
+    // Esta línea es opcional, previene el envío hasta que estés seguro de que funciona.
+    event.preventDefault();
+    alert('¡Gracias! Tu solicitud ha sido enviada. Te contactaremos en minutos.');
+});
